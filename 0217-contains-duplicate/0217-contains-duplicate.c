60f244c713975c1451151d1817b0b8f8098fe2e0
Saswata Adhikari <saswata.adhikari7243@gmail.com>
2026-09-21 17:37:37 +0530

    Time: 46 ms (77.49%), Space: 19.3 MB (40.86%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -0,0 +1,11 @@
+int cmp(const void *a, const void *b) {
+    int x = *(const int *)a, y = *(const int *)b;
+    return (x > y) - (x < y);   // avoids overflow from x - y
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 1; i < numsSize; i++)
+        if (nums[i] == nums[i - 1]) return true;
+    return false;
+}
